aa3b7114ac4a3398b280d495ee5caf8324591681
GianES26 <a20224926@pucp.edu.pe>
2025-05-09 20:21:51 -0500

    Creacion repositorio

---
+++ src/main/java/com/example/lab4_20224926_enriquezsoel/Lab420224926EnriquezSoelApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20224926_enriquezsoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420224926EnriquezSoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab420224926EnriquezSoelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab4_20224926_enriquezsoel/Lab420224926EnriquezSoelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20224926_enriquezsoel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420224926EnriquezSoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
